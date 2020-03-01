bb284801fc46f8470f61b23d87a349078a409487
Jesus Lozano <jalozano841@gmail.com>
2020-03-01 15:16:31 -0600

    Changed firebaseconfig

--- helpers/firebase.js
+++ helpers/firebase.js
@@ -6,11 +6,11 @@ import "firebase/firebase-auth"
 import * as firebaseui from "firebaseui"
 
 const firebaseConfig = {
-    apiKey:process.env.VUE_APP_apikey,
+    apiKey: process.env.VUE_APP_apiKey,
     authDomain: process.env.VUE_APP_authDomain,
     databaseURL: process.env.VUE_APP_databaseURL,
     projectId: process.env.VUE_APP_projectId,
-    storageBucket: process.env.VUE_APP_storeageBucket,
+    storageBucket: process.env.VUE_APP_storageBucket,
     messagingSenderId: process.env.VUE_APP_messagingSenderId,
     appId: process.env.VUE_APP_appId
 }
