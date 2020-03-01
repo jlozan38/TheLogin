15d255f3403194fea7d73a98427b9dc22cae69b3
Jesus Lozano <jalozano841@gmail.com>
2020-02-29 21:39:29 -0600

    NavBar and Public pages layout

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from "./router";
 import store from "./store";
 import "./assets/tailwind.css";
 
-import "./helpers/firebase";
+import "../helpers/firebase";
 import "firebaseui/dist/firebaseui.css"
 
 Vue.config.productionTip = false;
